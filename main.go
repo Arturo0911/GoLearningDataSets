fb0fac423701b113146a328c555c38dfd279d6a0
arturo <anegreiross@outlook.com>
2021-05-09 21:36:21 -0500

    Modeling plots

--- main.go
+++ main.go
@@ -2,41 +2,97 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/sjwhitworth/golearn/base"
-	"github.com/sjwhitworth/golearn/evaluation"
-	"github.com/sjwhitworth/golearn/knn"
+	"github.com/kniren/gota/dataframe"
 )
 
+const (
+	PATH_FILE      = "datasets/COVID.csv"
+	TRAIN_FILE     = "covid_train.csv"
+	TEST_FILE      = "covid_test.csv"
+	PATH_IMAGES    = "images/"
+	IRIS_PATH_FILE = "datasets/iris_headers.csv"
+)
+
+func checkMapSet(cases map[string]int, element string) bool {
+	var result bool = false
+	for key := range cases {
+		if key == element {
+			result = true
+			break
+		}
+	}
+	return result
+}
+
+func ReadingFile() {
+	file, err := os.Open(PATH_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	covidDF := dataframe.ReadCSV(file)
+
+	mapGeneral := make(map[string]map[string]int)
+
+	for _, element := range covidDF.Names() {
+
+		mapSet := make(map[string]int)
+		if element != "Timestamp" {
+			for _, value := range covidDF.Col(element).Records() {
+
+				if checkMapSet(mapSet, value) {
+					mapSet[value] += 1
+				} else {
+					mapSet[value] = 1
+				}
+			}
+			mapGeneral[element] = mapSet
+		}
+	}
+
+	for key, value := range mapGeneral {
+		fmt.Println(key, ": ", value)
+	}
+}
+
+// Reading and practicing about iris_file
+func MakingFilesIris() {
+
+	irisFile, err := os.Open(IRIS_PATH_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer irisFile.Close()
+
+	irisDF := dataframe.ReadCSV(irisFile)
+	fmt.Println(irisDF)
+
+	trainingNum := (4 * irisDF.Nrow()) / 5
+	testNum := (irisDF.Nrow()) / 5
+
+	if trainingNum+testNum < irisDF.Nrow() {
+		trainingNum++
+	}
+
+	// training sets
+	trainSet := make([]int, trainingNum)
+	testSet := make([]int, testNum)
+
+	for i := 0; i < trainingNum; i++ {
+		trainSet[i] = i
+	}
+
+	for j := 0; j < testNum; j++ {
+		testSet[j] = trainingNum + j
+	}
+
+}
+
 func main() {
-	// Load in a dataset, with headers. Header attributes will be stored.
-	// Think of instances as a Data Frame structure in R or Pandas.
-	// You can also create instances from scratch.
-	rawData, err := base.ParseCSVToInstances("examples/datasets/iris.csv", false)
-	if err != nil {
-		panic(err)
-	}
-
-	// Print a pleasant summary of your data.
-	fmt.Println(rawData)
-
-	//Initialises a new KNN classifier
-	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
-
-	//Do a training-test split
-	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
-	cls.Fit(trainData)
-
-	//Calculates the Euclidean distance and returns the most popular label
-	predictions, err := cls.Predict(testData)
-	if err != nil {
-		panic(err)
-	}
-
-	// Prints precision/recall metrics
-	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
-	}
-	fmt.Println(evaluation.GetSummary(confusionMat))
+	//ReadingFile()
+	MakingFilesIris()
 }
---
+++ plotters/plott_maker.go
@@ -0,0 +1,5 @@
+package plotters
+
+func MakerPlot() {
+
+}
